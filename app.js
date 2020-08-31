53cc70c5caab92ba9fb00b1bdd9c160512799e77
Michelle Hinojosa <bmontellano@gmail.com>
2020-08-31 10:48:52 -0700

    finished vanilla JS and html components

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const calculateBMIButton = document.getElementById("calculate-BMI-Button"),
       resetBMIButton = document.getElementById("reset-BMI"),
       heightInput = document.getElementById("height-input"),
-      weightInput = document.getElementById("weight-input");
+      weightInput = document.getElementById("weight-input"),
+      bmiResultArea = document.getElementById("bmi-result");
 
 const resetBMI = () => {
 heightInput.value = '';
@@ -12,7 +13,19 @@ const calculateBMI = () => {
   var enteredHeight = +heightInput.value,
       enteredWeight = +weightInput.value,
       bmi = enteredWeight / (Math.pow(enteredHeight,2));
-      console.log(bmi);
+
+      if(isNaN(bmi)) {
+        alert('Input not number');
+      } else {
+        const resultElement = document.createElement('ion-card');
+        resultElement.innerHTML = `
+        <ion-card-content>
+        <h2>${bmi}</h2>
+        </ion-card-content>
+        `;
+        bmiResultArea.innerHTML = '';
+        bmiResultArea.appendChild(resultElement);
+      }
 }
 
 calculateBMIButton.addEventListener('click', calculateBMI);
